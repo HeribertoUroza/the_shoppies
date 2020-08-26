d91d33e5764b2bdcdb775a2f4ec1905566fcc1a3
Heriberto Uroza <heribertouroza@pursuit.org>
2020-08-26 18:23:16 -0400

    added file to gitignore to hide api key

--- src/api/OMDB.js
+++ src/api/OMDB.js
@@ -1,9 +1,10 @@
 import Axios from 'axios';
+import apikey from './config';
 
 const getAPIdata = ( query ) => {
     return Axios({
         method: 'GET',
-        url: `http://www.omdbapi.com/?apikey=&t=${query}`
+        url: `http://www.omdbapi.com/?apikey=${apikey.omdb_key}&t=${query}`
     }).then( res => {
         console.log(res.data)
     }).catch( error => {
