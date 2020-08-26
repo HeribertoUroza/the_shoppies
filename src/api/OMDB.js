be555b87a3513961611109910b8680f359b1d69c
Heriberto Uroza <heribertouroza@pursuit.org>
2020-08-26 18:10:34 -0400

    tested api call

--- src/api/OMDB.js
+++ src/api/OMDB.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
 const getAPIdata = ( query ) => {
     return Axios({
         method: 'GET',
-        url: `http://www.omdbapi.com/?apikey=e33ebc2e&t=${query}`
+        url: `http://www.omdbapi.com/?apikey=&t=${query}`
     }).then( res => {
         console.log(res.data)
     }).catch( error => {
