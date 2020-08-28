37335f3af162aec8086931c8bd3c9c0e5e18c5cc
Heriberto Uroza <heribertouroza@pursuit.org>
2020-08-28 16:11:20 -0400

    unable to rerender. troubleshooting more

--- src/containers/ResultsSection.js
+++ src/containers/ResultsSection.js
@@ -1,22 +1,28 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, } from 'react';
 
 import ResultContext from '../context/ResultsContext';
 
 
 function ResultsSection() {
-    // let [ resultData, getResultData ] = useState([])
+    //let [ resultData, getResultData ] = useState([])
     let currentResults = useContext(ResultContext)
-    console.log(currentResults)
-    // useEffect(() => {
-        
-    // })
 
-    return(
+    function getContextData(){
+
+        return currentResults.results.map((e, i) => {
+            console.log(e)
+            return <h4>{e}</h4>
+        })
+    };
+
+    //getResultData(currentResults.results)
+    // console.log('CR',currentResults.results)
+    // console.log('results',results)
+    return (
         <>
+            Results
             {
-                currentResults.results ? currentResults.results.map((e,i)=> {
-                    return <h4>{e}</h4>
-                }): ''
+                getContextData()
             }
         </>
     )
