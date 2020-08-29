9171fee0e8f8610bffcd312fb722541d269a45a3
Heriberto Uroza <heribertouroza@pursuit.org>
2020-08-29 16:18:07 -0400

    attempted using consumer from context api. was able to get data correctly but it is not rerendering once data changes

--- src/containers/ResultsSection.js
+++ src/containers/ResultsSection.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
-import currentResults from '../context/ResultsContext';
+import ResultsContext from '../context/ResultsContext';
 
 
 function ResultsSection() {
@@ -7,15 +7,22 @@ function ResultsSection() {
 
     
     //getResultData(currentResults.results)
-    // console.log('CR',currentResults.results)
-    useEffect( _=> {
-        console.log('results', currentResults)
-
-    })
+    
+    
 
     return (
         <>
             Results
+            <ResultsContext.Consumer>
+                {
+                    ({results})=> {
+                        console.log(results)
+                        results.map((e,i) => {
+                            return <h3>{e}</h3>
+                        })
+                    }
+                }
+            </ResultsContext.Consumer>
             {/* {
                 for(let currentResults in props){
                     console.log(currentResults)
