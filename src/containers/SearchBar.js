0cc7b42ca70cef1c7b36f41c2c83397ed813f5bc
Heriberto Uroza <heribertouroza@pursuit.org>
2020-08-29 16:24:48 -0400

    updated styling for the search section, button not required

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -5,7 +5,6 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
 
 // API CALL
 import getAPIdata from '../api/OMDB';
@@ -19,10 +18,11 @@ function SearchBar() {
     
     function handleChange(e) {
         setQuery(e.target.value)
+
         if (query.length < 1) return;
         getAPIdata(query)
             .then(res => {
-                // console.log(`${res.data.Title} (${res.data.Year})`)
+                if(!res.data.Title && !res.data.Year) return;
                 currentResults.results.push(`${res.data.Title} (${res.data.Year})`)
             }).catch(error => {
                 console.log(error)
@@ -35,7 +35,6 @@ function SearchBar() {
                 <h1>The Shoppies</h1>
                 <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2 search-bar" onChange={handleChange}/>
-                    <Button variant="outline-primary search-btn">Search</Button>
                 </Form>
             </Container>
         </Jumbotron>
