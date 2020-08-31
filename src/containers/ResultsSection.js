91e1b46dcce1af3dbba2a8444d2b6b99f7f85922
Heriberto Uroza <heribertouroza@pursuit.org>
2020-08-31 18:48:44 -0400

    cleaned up code

--- src/containers/ResultsSection.js
+++ src/containers/ResultsSection.js
@@ -4,35 +4,17 @@ import ResultsContext from '../context/ResultsContext';
 
 function ResultsSection() {
     let [ resultData, getResultData ] = useState([])
-    // getResultData(ResultsContext.results)
-    // console.log(resultData)
     let currentResults = useContext(ResultsContext)
     
 
     useEffect(() => {
-        console.log(currentResults)
         getResultData(currentResults)
     }, [currentResults])
 
     return (
         <>
             Results
-            {/* <ResultsContext.Consumer>
-                {
-                    ({results})=> {
-                        if(results.length){
-                            results.map((e, i) => {
-                                console.log(e)
-                                return <h3>{e}</h3>
-                            })
-                        } else {
-                            return ""
-                        }
-                    }
-                }
-            </ResultsContext.Consumer> */}
             {
-               
                 resultData.map((e, i) => {
                     return <h3>{e.Title}</h3>
                 })
