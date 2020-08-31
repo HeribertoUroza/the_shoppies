f9947abe96be9f52d297b3b53bcc2e1fd86d88aa
Heriberto Uroza <heribertouroza@pursuit.org>
2020-08-31 18:36:21 -0400

    moved functions from search bar container to app.js to better handle render

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // CONTAINERS
 import SearchBar from './containers/SearchBar';
 import ResultsSection from './containers/ResultsSection';
 
 // CONTEXT
-import ResultsContext, { results } from './context/ResultsContext'
+import ResultsContext from './context/ResultsContext'
+
+// API CALL
+import getAPIdata from './api/OMDB';
 
 // CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
 
 function App() {
+  const [ results, getResults ] = useState([])
+  const [ preResults, setPreResults] = useState([])
+
+  const apiCall = async (query) => {  
+  const res = await getAPIdata('s',query)
+  
+  if((preResults.length > 0 && preResults === res.Search)) {
+    const getMovieTitle = await getAPIdata('t', query)
+    if(getMovieTitle.Title)
+    getResults(getMovieTitle)
+    }
+  if(res.Search){
+    getResults(res.Search)
+    setPreResults(res.Search)
+    } 
+  }
+
   return (
     <>
       <ResultsContext.Provider value={results}>
-        <SearchBar />
+        <SearchBar apiCall={apiCall} />
         <ResultsSection />
       </ResultsContext.Provider>
     </>
