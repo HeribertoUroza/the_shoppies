b6e9a9b3607bac2381b5953040c10614994deec1
Heriberto Uroza <heribertouroza@pursuit.org>
2020-09-03 17:25:22 -0400

    some styling added, adding toggle function next

--- src/containers/NomiSection.js
+++ src/containers/NomiSection.js
@@ -5,6 +5,11 @@ import React, { useState, useEffect, useContext } from 'react';
 // CONTEXT
 import NomiContext from '../context/NomiContext';
 
+// BOOTSTRAP IMPORTS
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
 
 function NomiSection(props){
     let [ currNomiData, getNomiData ] = useState([]);
@@ -17,12 +22,22 @@ function NomiSection(props){
 
     return (
         <>
-            <h3>Nominations</h3>
-        {
-            currNomiData.map((e, i) => {
-                return <h3>{e}</h3>
-            })
-        }
+            <Jumbotron fluid>
+                <Container>
+                    <h3>Nominations</h3>
+                    <ListGroup variant='flush'>
+                        {
+                            currNomiData.map((e, i) => {
+                                return (
+                                    <ListGroup.Item key={i}>{e}
+                                        <Button variant="outline-primary" >Remove</Button>
+                                    </ListGroup.Item>
+                                ) 
+                            })
+                        }            
+                    </ListGroup>
+                </Container>
+            </Jumbotron>
         </>
     )
 }
