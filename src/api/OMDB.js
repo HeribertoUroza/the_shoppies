1b8caba5afb6c1df28c3689b6d94382e35c94566
Heriberto Uroza <heribertouroza@pursuit.org>
2020-09-04 11:14:21 -0400

    troubleshooting api call on live site

--- src/api/OMDB.js
+++ src/api/OMDB.js
@@ -2,9 +2,10 @@ import Axios from 'axios';
 //import apikey from './config';
 
 const getAPIdata = async ( type, query ) => {
+    const key = process.env.REACT_APP_OMDB
     const api = await Axios({
         method: 'GET',
-        url: `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB}&${type}=${query}`
+        url: `http://www.omdbapi.com/?apikey=${key}&${type}=${query}`
     })
     
     return api.data
