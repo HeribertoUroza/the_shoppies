e660185be64591c9e842e70d3e86a22a43528097
Heriberto Uroza <heribertouroza@pursuit.org>
2020-09-04 10:57:45 -0400

    added hidden key into netlify

--- src/api/OMDB.js
+++ src/api/OMDB.js
@@ -4,7 +4,7 @@ import apikey from './config';
 const getAPIdata = async ( type, query ) => {
     const api = await Axios({
         method: 'GET',
-        url: `http://www.omdbapi.com/?apikey=${apikey.omdb_key}&${type}=${query}`
+        url: `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB}&${type}=${query}`
     })
     
     return api.data
